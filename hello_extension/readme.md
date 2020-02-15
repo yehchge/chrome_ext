e76314e2982d20a3d4affc69dc0789c80affea3b
bill.yeh <bill.yeh@iwant-in.net>
2020-02-15 13:37:52 +0800

    chrome ext test

---
+++ hello_extension/readme.md
@@ -0,0 +1,7 @@
+https://developer.chrome.com/extensions
+
+導航到chrome://extensions您的瀏覽器。
+您還可以通過單擊多功能框右上角的Chrome菜單，將鼠標懸停在“ 更多工具”上並選擇“ 擴展”來訪問此頁面。
+選中開發者模式旁邊的框。
+單擊Load Unpacked Extension並選擇“Hello Extensions”擴展名的目錄。
+恭喜！您現在可以通過單擊hello_world.png圖標或按 Ctrl+Shift+F鍵盤來使用基於彈出式的擴展。
---
+++ proxy/popup.js
@@ -0,0 +1,68 @@
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    chrome.tabs.executeScript({
+      code: 'console.log("dddd")'
+    });
+    message.innerText = request.source;
+    // var mydata = JSON.parse(request.source);
+    // alert(mydata);
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+
+function getJsonData(){
+  var message = document.querySelector('#message');
+  $.ajax({
+      method: "GET",
+      dataType: "json",
+      url: "https://www.dcard.tw/_api/forums/makeup/posts?popular=false"
+  }).done(function(msg) {
+      // chrome.tabs.executeScript({
+      //   code: 'console.log("' + JSON.stringify(msg) + '")'
+      // });
+
+      var ss = '<table border=1>';
+      for(k in msg){
+        ss += '<tr><td>';
+        ss += 'id: ' + msg[k].id + ";";
+        ss += '</td><td>';
+        ss += 'title: <a href="https://www.dcard.tw/f/makeup/p/' + msg[k].id + '" target="_BLANK">' + msg[k].title + "</a>";
+        ss += '</td></tr>';
+      }
+      ss += '</table>';
+      message.innerHTML = ss;
+  });
+}
+
+function myrun(){
+  chrome.tabs.executeScript({
+    code: 'console.log("run myrun")'
+  });
+  var result = getJsonData();
+}
+
+
+let getme = document.getElementById('getme');
+getme.onclick = function(element){
+  chrome.tabs.executeScript({
+    code: 'console.log("click")'
+  });
+  myrun();
+}
+
+// window.onload = onWindowLoad;
